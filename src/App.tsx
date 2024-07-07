f85390edde94af276af11b2698ab2838c18ca20c
Frank Fox <ffox77@gmail.com>
2024-07-07 14:39:54 -0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="w-full h-full m-8 items-center flex gap-6">
+        <div className="border-l h-[90px]">&nbsp;</div>
+        <div className="flex gap-3 items-end">
+          <div className="text-white text-6xl font-black">SILK</div>
+          <div className="text-white text-4xl mb-1">network</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
